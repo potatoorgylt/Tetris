08d5e753d15b85cd027c3683bd41a753c161871c
potatoorgylt <gynejas13@gmail.com>
2018-08-29 11:17:01 +0300

    First files

---
+++ Assets/Scripts/BlockBehaviour.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockBehaviour : MonoBehaviour {
+
+    Level level;
+    Vector3 curPos;
+    float step = 1;
+
+    public Transform[] blocks;
+
+    [HideInInspector]
+    public int transformCase = 0;
+
+    [System.Serializable]
+    public class MultiVector3
+    {
+        public Vector3[] blockTransforms = new Vector3[4];
+    }
+    public MultiVector3[] rotations;
+
+    void Start () {
+        level= GameObject.FindGameObjectWithTag("Level").GetComponent<Level>();
+        curPos = transform.position;
+        StartCoroutine(MoveDown());
+	}
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("left"))
+        {
+            transform.Translate(new Vector3(-1, 0, 0), Space.World);
+
+        }
+        else if (Input.GetKeyDown("right"))
+        {
+            transform.Translate(new Vector3(1, 0, 0), Space.World);
+        }
+        else if (Input.GetKeyDown("up"))
+        {
+            RotateBlocks();
+        }
+        else if (Input.GetKeyDown("down"))
+        {
+            //Move faster
+        }
+    }
+
+    void RotateBlocks()
+    {
+        for(int j = 0; j < rotations[transformCase].blockTransforms.Length; j++)
+        {
+            blocks[j].transform.localPosition = rotations[transformCase].blockTransforms[j];
+        }
+        transformCase++;
+        if(transformCase >= rotations.Length)
+        {
+            transformCase = 0;
+        }
+    }
+
+    IEnumerator MoveDown()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(level.moveTime);
+            curPos.y = step * (-1);
+            transform.Translate(curPos, Space.World);
+        }
+    }
+}
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+
+    public float moveTime = 1;
+
+    public int gridWidth;
+    public int gridLength;
+
+    public Vector3[,] grid;
+
+    private void Start()
+    {
+        InitializeGrid();
+    }
+
+    void InitializeGrid()
+    {
+        float posX = gridWidth / 2 * (-1);
+        float posY = gridLength / 2 * (-1);
+
+        grid = new Vector3[gridLength, gridWidth];
+
+        for (int i = 0; i < gridLength; i++)
+        {
+            for (int j = 0; j < gridWidth-1; j++)
+            {
+                posX++;
+                grid[i, j] = new Vector3(posX, posY, 0);
+                Debug.Log(grid[i, j]);
+            }
+            posX = gridWidth / 2 * (-1);
+            posY++;
+        }
+    }
+}
